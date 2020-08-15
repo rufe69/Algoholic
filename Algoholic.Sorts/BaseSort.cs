fc27d8c35da8c427bc6c70ce17c0097fda694710
Кирилл Бушманов <bka@pkazdrav.ru>
2020-08-15 22:37:45 +0300

    Создал первоначальный проект

---
+++ Algoholic.Sorts/BaseSort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algoholic.Sorts
+{
+    public abstract class BaseSort
+    {
+        public virtual IEnumerable<IComparable> Sort(IEnumerable<IComparable> collections)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Algoholic/Program.cs
@@ -0,0 +1,13 @@
+﻿using Algoholic.Sorts;
+using System;
+
+namespace Algoholic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
